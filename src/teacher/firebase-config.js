338945ce1feed7e39c72d13866efa77d23a65db1
Francisco Pereira de Lima Local <frped2@MC2001343.local>
2024-10-30 14:53:41 -0400

    Ajout Firebase au fichier index Teacher

---
+++ src/teacher/firebase-config.js
@@ -0,0 +1,15 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBVAVVcpmicOnyXYBydEW4KfCu0B0ukNe4",
+  authDomain: "fale-comigo-d4522.firebaseapp.com",
+  projectId: "fale-comigo-d4522",
+  storageBucket: "fale-comigo-d4522.firebasestorage.app",
+  messagingSenderId: "464958131394",
+  appId: "1:464958131394:web:547ac8416d49f8d31d3f5b",
+  measurementId: "G-7XPJ5MHWYX"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+// Initialize Firestore
+const db = firebase.firestore();
