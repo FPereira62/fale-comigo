e475593b42b9fef1dd8f6c801dd5ea103c070de8
Francisco Pereira de Lima Local <frped2@MC2001343.local>
2024-11-24 17:55:16 -0500

    Mise à jour fichier firebase-config

--- src/teacher/firebase-config.js
+++ src/teacher/firebase-config.js
@@ -1,14 +1,17 @@
 // Configuration Firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyBVAVVcpmicOnyXYBydEW4KfCu0B0ukNe4",
-  authDomain: "fale-comigo-d4522.firebaseapp.com",
-  projectId: "fale-comigo-d4522",
-  storageBucket: "fale-comigo-d4522.firebasestorage.app",
-  messagingSenderId: "464958131394",
-  appId: "1:464958131394:web:547ac8416d49f8d31d3f5b",
-  measurementId: "G-7XPJ5MHWYX"
+    apiKey: "AIzaSyBVAVVcpmicOnyXYBydEW4KfCu0B0ukNe4",
+    authDomain: "YOUR_AUTH_DOMAIN",
+    projectId: "YOUR_PROJECT_ID",
+    storageBucket: "YOUR_STORAGE_BUCKET",
+    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+    appId: "YOUR_APP_ID",
+    measurementId: "YOUR_MEASUREMENT_ID"
 };
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
+
+// Exportation pour utilisation dans d'autres fichiers
+window.db = db;
