d1d2fedeab759784b35e09b5f85d0a39a4d52042
FPereira62 <francisco.pereira@hotmail.ca>
2024-12-09 22:44:18 -0500

    Création d'un fichier firebase-config partagé

---
+++ public/js/firebase-config.js
@@ -0,0 +1,12 @@
+// Configuration Firebase partagée entre les interfaces
+const firebaseConfig = {
+    apiKey: "AIzaSyBVAVVcpmicOnyXYBydEW4KfCu0B0ukNe4",
+    authDomain: "fale-comigo-d4522.firebaseapp.com",
+    projectId: "fale-comigo-d4522",
+    storageBucket: "fale-comigo-d4522.firebasestorage.app",
+    messagingSenderId: "464958131394",
+    appId: "1:464958131394:web:547ac8416d49f8d31d3f5b"
+};
+
+// Initialisation de Firebase
+firebase.initializeApp(firebaseConfig);
